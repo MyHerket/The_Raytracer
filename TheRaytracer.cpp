fe90bad369985c68f780648508208240fce23a92
MYRIAM HERNÁNDEZ KETCHUL <myriam.hernandez@cimat.mx>
2020-12-11 02:12:09 -0600

    Complete chapter 6 implementation: Diffusion

--- TheRaytracer.cpp
+++ TheRaytracer.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <cfloat>
 #include "hitable_list.h"
+#include "Camera.h"
 
 vec3 color(const ray& r, hitable* world) {
     hit_record rec; 
@@ -24,7 +25,8 @@ vec3 color(const ray& r, hitable* world) {
 int main()
 {
     int nx = 200; 
-    int ny = 100; 
+    int ny = 100;
+    int ns = 100;
     std::ofstream file("image.ppm"); 
     if (!file.is_open()) {
         std::cout << "Error de apertura de archivo\n";
@@ -32,26 +34,25 @@ int main()
     }
 
     file << "P3\n" << nx << " " << ny << "\n255" << std::endl; 
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
     
     hitable* list[2]; 
     list[0] = new sphere(vec3(-0.7, 0, -1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1.5), 100); 
     hitable* world = new hitable_list(list, 2);
-
+    camera cam;
 
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            vec3 dir = lower_left_corner + (u * horizontal) + (v * vertical);
+            vec3 col(0.0, 0.0, 0.0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + float(rand() % 100) / 100.0) / float(nx);
+                float v = float(j + float(rand() % 100) / 100.0) / float(ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col += color(r, world);
+            }
 
-            ray r(origin, dir);
-            vec3 col = color(r, world);
-
+            col /= float(ns);
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
