1b0c5484c7ffaf6958d9a3a3efbc04d505e01275
MYRIAM HERNÁNDEZ KETCHUL <myriam.hernandez@cimat.mx>
2020-12-10 21:25:20 -0600

    End Chapter2

--- TheRaytracer.cpp
+++ TheRaytracer.cpp
@@ -2,7 +2,7 @@
 //
 
 #include <iostream>
-#inclide "Vec3_Class.h"
+#include "Vec3_Class.h"
 
 int main()
 {
@@ -11,12 +11,10 @@ int main()
     std::cout << "P3\n" << nx << " " << ny << "\n255\n"; 
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = nx - 1; i >= 0; i--) {
-            float r = float(i) / float(nx);
-            float g = float(j) / float(ny);
-            float b = 0.2; 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
             std::cout << ir << " " << ig << " " << ib << "\n"; 
         }
     }
