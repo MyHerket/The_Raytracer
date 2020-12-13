0ceb2b301e1546ebf697a38a67507a4b08e10e40
MYRIAM HERNÁNDEZ KETCHUL <myriam.hernandez@cimat.mx>
2020-12-12 22:05:05 -0600

    Camera Issue but better colors

--- TheRaytracer.cpp
+++ TheRaytracer.cpp
@@ -37,14 +37,14 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 200; 
     const int image_height = static_cast<int>(image_width/aspect_ratio);
-    const int samples_per_pixel = 50;
+    const int samples_per_pixel = 100;
     const int max_depth = 10;
 
     //World
     hitable_list world;
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5));
-    world.add(make_shared<sphere>(point3(1.0, 1.0, -1.0), 0.5));
-    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100));
+    world.add(make_shared<sphere>(point3(-0.7, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(1, 0.7, -1.5), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1.5), 100));
 
 
     //Setup Camera
@@ -68,6 +68,7 @@ int main()
                 auto u = (i + random_double()) / (image_width - 1.0);
                 auto v = (j + random_double()) / (image_height - 1.0);
                 ray r = cam.get_ray(u, v);
+                vec3 p = r.at(2.0);
                 pixel_color += ray_color(r, world, max_depth);
             }
             write_color(file, pixel_color, samples_per_pixel);
