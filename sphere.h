31fee34b11f6c2f7c5032032c1b88eb6bc471673
MYRIAM HERNÁNDEZ KETCHUL <myriam.hernandez@cimat.mx>
2020-12-12 23:07:58 -0600

    Perfect Just like the tutorial

--- sphere.h
+++ sphere.h
@@ -1,5 +1,5 @@
 #pragma once
-/*#ifndef SPHEREH 
+#ifndef SPHEREH 
 #define SPHEREH
 
 #include "hitable.h"
@@ -8,12 +8,13 @@
 class sphere : public hitable {
 public:
     sphere() {}
-    sphere(point3 cen, double r) : center(cen), radius(r) {};
+    sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r), mat_ptr(m) {};
     virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const override;
 
 public:
     point3 center;
     double radius;
+    shared_ptr<material> mat_ptr;
 };
 
 bool sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const {
@@ -27,9 +28,7 @@ bool sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const 
     double sqrtd = sqrt(discriminant); 
 
     double root = (-half_b - sqrtd) / a; 
-    if (root < tmin || tmax < root) {
-        root = (-half_b + sqrtd) / a; 
-        if (root < tmin || tmax < root)
+    if (!(tmax> root && root > tmin)){
             return false;
     }
 
@@ -41,41 +40,5 @@ bool sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const 
 
     return true;
 }
- */
-#ifndef SPHEREH 
-#define SPHEREH
 
-#include "hitable.h"
-
-class sphere : public hitable {
-public:
-    sphere();
-    sphere(point3 cen, double r, shared_ptr<material> m) : center(cen), radius(r),  mat_ptr(m) {};
-    virtual bool hit(const ray& r, double tmin, double tmax, hit_record& rec) const override;
-
-public:
-    point3 center;
-    float radius;
-    shared_ptr<material> mat_ptr;
-};
-
-bool sphere::hit(const ray& r, double tmin, double tmax, hit_record& rec) const {
-    vec3 oc = r.origin() - center;
-    double a = dot(r.direction(), r.direction());
-    double b = dot(oc, r.direction());
-    double c = dot(oc, oc) - (radius * radius);
-    double discriminant = (b * b) - (a * c);
-
-    if (discriminant > 0.0) {
-        float temp = (double(-b) - sqrt(discriminant)) / (a);
-        if (temp < tmax && temp > tmin) {
-            rec.t = temp;
-            rec.p = r.at(rec.t);
-            vec3 outward_normal = (rec.p - center) / radius; 
-            rec.set_face_normal(r, outward_normal);
-            return true;
-        }
-    }
-    return false;
-}
-#endif // !SPHEREH 
+#endif 
