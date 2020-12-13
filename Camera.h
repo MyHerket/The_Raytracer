eb0f95b37087d05bf9a585eb717df61f552cd134
MYRIAM HERNÁNDEZ KETCHUL <myriam.hernandez@cimat.mx>
2020-12-12 21:34:30 -0600

    Nothing

--- Camera.h
+++ Camera.h
@@ -15,12 +15,12 @@ public:
 		origin = point3(0.0, 0.0, 0.0);
 		horizontal = vec3(viewport_width, 0.0, 0.0);
 		vertical = vec3(0.0, viewport_height, 0.0);
-		lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0.0, 0.0, focal_lenght);
+		lower_left_corner = origin - (horizontal / 2) - (vertical / 2) - vec3(0.0, 0.0, focal_lenght);
 	}
 
 
 	ray get_ray(double u, double v) const { 
-		return ray(origin, lower_left_corner + (u * horizontal) + (v * vertical)-origin); 
+		return ray(origin, lower_left_corner + (u * horizontal) + (v * vertical)- origin); 
 	}
 
 private:
