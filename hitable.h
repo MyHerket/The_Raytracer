5816c0b63f3fdf79ed07327cb09d2978cd5b554d
MYRIAM HERNÁNDEZ KETCHUL <myriam.hernandez@cimat.mx>
2020-12-16 14:36:27 -0600

    rotation z axis

--- hitable.h
+++ hitable.h
@@ -59,6 +59,8 @@ bool translate::bounding_box(double time0, double time1, aabb& output_box) const
 	return true;
 }
 
+
+//Rotation y axis
 class rotate_y : public hitable {
 public: 
 	rotate_y(shared_ptr<hitable> p, double angle);
@@ -136,6 +138,89 @@ bool rotate_y::hit(const ray& r, double t_min, double t_max, hit_record& rec)con
 
 	rec.p = p; 
 	rec.set_face_normal(rotated_r, normal); 
+	return true;
+
+}
+
+
+//Rotation z_axis
+class rotate_z : public hitable {
+public:
+	rotate_z(shared_ptr<hitable> p, double angle);
+
+	virtual bool hit(
+		const ray& r, double t_min, double t_max, hit_record& rec
+	)const override;
+
+	virtual bool bounding_box(double time0, double time1, aabb& output_box) const override {
+		output_box = bbox;
+		return hasbox;
+	}
+
+
+	shared_ptr<hitable> ptr;
+	double sin_theta;
+	double cos_theta;
+	bool hasbox;
+	aabb bbox;
+};
+
+rotate_z::rotate_z(shared_ptr<hitable> p, double angle) : ptr(p) {
+	auto radians = degrees_to_radians(angle);
+	sin_theta = sin(radians);
+	cos_theta = cos(radians);
+	hasbox = ptr->bounding_box(0, 1, bbox);
+
+	point3 min(infinity, infinity, infinity);
+	point3 max(-infinity, -infinity, -infinity);
+
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 2; j++) {
+			for (int k = 0; k < 2; k++) {
+				auto x = double(i) * bbox.max().x() + (1.0 - double(i)) * bbox.min().x();
+				auto y = double(j) * bbox.max().y() + (1.0 - double(j)) * bbox.min().y();
+				auto z = double(k) * bbox.max().z() + (1.0 - double(k)) * bbox.min().z();
+
+				auto newx = cos_theta * x + sin_theta * y;
+				auto newy = -sin_theta * x + cos_theta * y;
+
+				vec3 tester(newx, newy, z);
+
+				for (int c = 0; c < 3; c++) {
+					min[c] = fmin(min[c], tester[c]);
+					max[c] = fmax(max[c], tester[c]);
+				}
+			}
+		}
+	}
+
+	bbox = aabb(min, max);
+}
+
+bool rotate_z::hit(const ray& r, double t_min, double t_max, hit_record& rec)const {
+	auto origin = r.origin();
+	auto direction = r.direction();
+	origin[0] = cos_theta * r.origin()[0] - sin_theta * r.origin()[1];
+	origin[1] = sin_theta * r.origin()[0] + cos_theta * r.origin()[1];
+
+	direction[0] = cos_theta * r.direction()[0] - sin_theta * r.direction()[1];
+	direction[1] = sin_theta * r.direction()[0] + cos_theta * r.direction()[1];
+
+	ray rotated_r(origin, direction, r.time());
+	if (!ptr->hit(rotated_r, t_min, t_max, rec))
+		return false;
+
+	auto p = rec.p;
+	auto normal = rec.normal;
+
+	p[0] = cos_theta * rec.p[0] + sin_theta * rec.p[1];
+	p[1] = -sin_theta * rec.p[0] + cos_theta * rec.p[1];
+
+	normal[0] = cos_theta * rec.normal[0] + sin_theta * rec.normal[1];
+	normal[1] = -sin_theta * rec.normal[0] + cos_theta * rec.normal[1];
+
+	rec.p = p;
+	rec.set_face_normal(rotated_r, normal);
 	return true;
 
 }
