4c2347b65639ebcd1614e2f8305b6cc98812c72a
MYRIAM HERNÁNDEZ KETCHUL <myriam.hernandez@cimat.mx>
2020-12-22 13:49:02 -0600

    Doesnt Work

--- TheRaytracer.cpp
+++ TheRaytracer.cpp
@@ -57,6 +57,7 @@ int main()
 
     auto chess = make_shared<checker_texture>(vec3(0.0, 0.0, 0.0), vec3(1.0, 1.0, 1.0));
     Sample_Scene.loadMesh("cubo", "cube2.obj", make_shared<lambertian>(chess));
+
     Sample_Scene.nh_sphere("Esfera_vidrio", point3(2, 2, 2), 3, glass);
 
     Sample_Scene.render("Something", "image3.ppm", 300, 300, point3(26, 3, 6), point3(0, 0, 0), vec3(0, 1, 0), 20.0, color(0.5, 0.5, 0.5), lights);
