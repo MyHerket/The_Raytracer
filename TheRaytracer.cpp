b105accdae2391e56219d698f806102d88c0a38a
Myriam Hernández Ketchul <myriam.hk@hotmail.com>
2020-12-22 19:37:11 -0700

    Intentar con la suma

--- TheRaytracer.cpp
+++ TheRaytracer.cpp
@@ -12,19 +12,21 @@
 #include "render.h"
 int main()
 {
-
-    scene Sample_Scene;
-
+    //Materiales y Texturas
     auto difflight = make_shared<diffuse_light>(color(4, 4, 4));
-    auto lights = make_shared<hitable_list>();
-    lights->add(make_shared<xy_rect>(3, 5, 1, 3, -6, difflight));
-    lights->add(make_shared<xz_rect>(0, 5, 1, 5, 6, difflight));
-
     auto pertext = make_shared<noise_texture>(6);
     auto red = make_shared<lambertian>(color(0.65, 0.05, 0.05));
     auto chess = make_shared<checker_texture>(color(1, 1, 1), color(0.05, 0.05, 0.7));
     auto metal_blue = make_shared<metal>(chess, 0.5);
     auto metal_red = make_shared<metal>(color(0.7, 0.05, 0.05), 0.7);
+
+    //Escena1
+    /*scene Sample_Scene;
+    auto lights = make_shared<hitable_list>();
+    lights->add(make_shared<xy_rect>(3, 5, 1, 3, -6, difflight));
+    lights->add(make_shared<xz_rect>(0, 5, 1, 5, 6, difflight));
+
+    
     Sample_Scene.nh_sphere("Ground", point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext));
 
     Sample_Scene.add("Light1", make_shared<xy_rect>(3, 5, 1, 3, -6, difflight));
@@ -46,7 +48,13 @@ int main()
     Sample_Scene.nh_box("Cubo", point3(10,0,1), 1.0, metal_red);
     Sample_Scene.nh_box("Cubo_cristal", point3(10.1,0.1,1.1), 1.1, make_shared<dielectric>(1.3));
 
-    Sample_Scene.render("Something", "image3.ppm", 600, 300, point3(26, 3, 6), point3(0, 2, 0), vec3(0, 1, 0), 20.0, color(0.1, 0.1, 0.1), lights);
+    Sample_Scene.render("Something", "image3.ppm", 600, 300, point3(26, 3, 6), point3(0, 2, 0), vec3(0, 1, 0), 20.0, color(0.1, 0.1, 0.1), lights);*/
+
+    scene Ball;
+    Ball.nh_sphere("Ball", point3(0, 0, 0), 1, make_shared<basic>(color(0.7, 1.0, 0.7), 0.5, 0.7, 0.5));
+    Ball.light("Lamp1", point3(2, 2, 2), color(4, 4, 4), vec3(1, 0, 0));
+    auto light = make_shared<hitable_list>(make_shared<spotlight>(point3(2,2,2), color(4, 4,4), vec3(1, 0, 0)));
+    Ball.render2("Scene", "Ball.ppm", 400, 200, point3(26, 3, 6), point3(0, 0, 0), vec3(0, 1, 0), 20.0, color(0.1, 0.1, 0.1), light);
     
 
 
--- render.h
+++ render.h
@@ -51,6 +51,48 @@ color background(const ray& r) {
 
 }
 
+color ray_color2(
+	const ray& r, const color& ambient, const hitable& world, int depth, int first_depth, shared_ptr<hitable_list> lights
+) {
+	hit_record rec;
+	if (world.hit(r, 0.001, infinity, rec)) {
+		
+		//color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+		ray scattered;
+		color attenuation;
+		//Calculo de luz ambiente
+		rec.mat_ptr->scatter(r, rec, attenuation, scattered);
+		color ambient = rec.mat_ptr->ambient_coef() * ambient * attenuation;
+
+
+		//Calculo de luz especular y difusa
+		color specular(0, 0, 0);
+		color difuse(0, 0, 0);
+		point3 L;
+		vec3 v = -r.direction();
+		vec3 R = unit_vector(reflect(v, rec.normal));
+		double cos_alpha;
+		double cos_theta;
+		color intensity;
+		int exponent = 2.0;
+		double difuse_coef = rec.mat_ptr->difuse_coef();
+		double specular_coef = rec.mat_ptr->specular_coef();
+
+		for (const auto& lamp : lights->objects) {
+			L = unit_vector(lamp->get_center());
+			intensity = lamp->get_intensity();
+			cos_alpha = dot(L, R);
+			cos_theta = dot(L, unit_vector(rec.normal));
+			specular += specular_coef * pow(cos_alpha, exponent) * intensity;
+			difuse += difuse_coef * cos_theta * intensity;
+		}
+
+		return ambient+specular+difuse;
+	}
+	else if (depth == first_depth)
+		return background(r);
+}
+
 color ray_color(
 	const ray& r, const color& ambient, const hitable& world, int depth, int first_depth, shared_ptr<hitable_list> lights) {
 	hit_record rec;
@@ -215,6 +257,51 @@ public:
 					ray r = cam.get_ray(u, v);
 					vec3 p = r.at(2.0);
 					pixel_color += ray_color(r, ambient, world, max_depth, max_depth, lights);
+				}
+				write_color(file, pixel_color, samples_per_pixel);
+
+			}
+		}
+
+
+
+		std::cout << "\nDone\n";
+	}
+	void render2(
+		const char* node, const char* filename, int w, int h, const point3& eye, const point3& view,
+		const point3& up, double fov, const color& ambient, shared_ptr<hitable_list> lights) {
+
+		//Other Parameters	
+		int samples_per_pixel = 100;
+		const int max_depth = 50;
+		auto aspect_ratio = w / h;
+		auto aperture = 0.0;
+
+		//Setup Camera
+		auto disk_to_focus = 10.0;
+		camera cam(eye, view, up, fov, aspect_ratio, aperture, disk_to_focus, 0.0, 1.0);
+
+
+		//Render
+
+		std::ofstream file(filename);
+		if (!file.is_open()) {
+			std::cout << "Error de apertura de archivo\n";
+			exit(1);
+		}
+
+		file << "P3\n" << w << " " << h << "\n255\n";
+
+		for (int j = h - 1; j >= 0; --j) {
+			std::cout << "\rScanlines remaining: " << j << " " << std::flush;
+			for (int i = 0; i < w; ++i) {
+				color pixel_color(0.0, 0.0, 0.0);
+				for (int s = 0; s < samples_per_pixel; ++s) {
+					auto u = (i + random_double()) / (w - 1.0);
+					auto v = (j + random_double()) / (h - 1.0);
+					ray r = cam.get_ray(u, v);
+					vec3 p = r.at(2.0);
+					pixel_color += ray_color2(r, ambient, world, max_depth, max_depth, lights);
 				}
 				write_color(file, pixel_color, samples_per_pixel);
 
